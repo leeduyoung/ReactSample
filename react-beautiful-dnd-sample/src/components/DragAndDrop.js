c816d0b79ee57de14dd699b004a56ccb5c534b07
dylee <leeduyoung2002@gmail.com>
2020-02-06 22:20:43 +0900

    add react-beautiful-dnd

---
+++ react-beautiful-dnd-sample/src/components/DragAndDrop.js
@@ -0,0 +1,97 @@
+import React, { useState } from "react";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+
+const DragAndDrop = () => {
+    const [items, setItems] = useState(getItems(10));
+    console.log(items);
+
+    const onDragEnd = result => {
+        console.log('result: ', result)
+        if (!result.destination) return;
+
+        let datas = reorder(
+            items,
+            result.source.index,
+            result.destination.index
+        );  
+        console.log('datas: ', datas)
+
+        setItems(datas);
+    };
+
+    return (
+        <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="droppable">
+                {(provided, snapshot) => (
+                    <div
+                        ref={provided.innerRef}
+                        style={getListStyle(snapshot.isDraggingOver)}
+                        {...provided.droppableProps}
+                    >
+                        {items.map((item, index) => (
+                            <Draggable key={item.id} 
+                                draggableId={item.id} 
+                                index={index}>
+                                {(provided, snapshot) => (
+                                    <div>
+                                        <div
+                                            ref={provided.innerRef}
+                                            {...provided.dragHandleProps}
+                                            {...provided.draggableProps}
+                                            style={getItemStyle(
+                                                provided.draggableProps.style,
+                                                snapshot.isDragging
+                                            )}
+                                        >
+                                            {item.content}
+                                        </div>
+                                        {provided.placeholder}
+                                    </div>
+                                )}
+                            </Draggable>
+                        ))}
+                        {provided.placeholder}
+                    </div>
+                )}
+            </Droppable>
+        </DragDropContext>
+    );
+};
+
+export default DragAndDrop;
+
+// fake data generator(가짜 데이터 제너레이터)
+const getItems = count =>
+    Array.from({ length: count }, (v, k) => k).map(k => ({
+        id: `item-${k}`,
+        content: `item ${k}`
+    }));
+
+// a little function to help us with reordering the result(결과 재정렬을 돕는 함수)
+const reorder = (list, startIndex, endIndex) => {
+    const result = Array.from(list);
+    const [removed] = result.splice(startIndex, 1);
+    result.splice(endIndex, 0, removed);
+
+    return result;
+};
+
+// using some little inline style helpers to make the app look okay(보기좋게 앱을 만드는 인라인 스타일 헬퍼)
+const grid = 8;
+const getItemStyle = (draggableStyle, isDragging) => ({
+    // some basic styles to make the items look a bit nicer(아이템을 보기 좋게 만드는 몇 가지 기본 스타일)
+    userSelect: "none",
+    padding: grid * 2,
+    marginBottom: grid,
+
+    // change background colour if dragging(드래깅시 배경색 변경)
+    background: isDragging ? "lightgreen" : "grey",
+
+    // styles we need to apply on draggables(드래그에 필요한 스타일 적용)
+    ...draggableStyle
+});
+const getListStyle = isDraggingOver => ({
+    background: isDraggingOver ? "lightblue" : "lightgrey",
+    padding: grid,
+    width: 250
+});
