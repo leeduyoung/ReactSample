c0d1eabaa6eaa2cde001867637af45b2bc7affda
DY.lee <leeduyoung2002@gmail.com>
2020-02-26 21:45:21 +0900

    Delete config.js

--- react-apollo-app/src/configs/config.js
+++
@@ -1,8 +0,0 @@
-const debug = process.env.REACT_APP_BRANCH === "dev"
-
-module.exports = {
-  debug,
-  apiUrl: debug
-    ? "https://plus-api-dev.the8days.com/graphql"
-    : "https://plus-api-release.the8days.com/graphql",
-}
