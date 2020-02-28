939c286a635ddd6518adce503e12d94edf4898ef
Jade <leeduyoung2002@gmail.com>
2020-02-28 15:45:40 +0900

    add clsx plugin

--- react-apollo-app/src/pages/Home.js
+++ react-apollo-app/src/pages/Home.js
@@ -2,16 +2,27 @@ import React from "react";
 import "./Home.css";
 import { useQuery } from "@apollo/react-hooks";
 import { CATEGORY_QUERY } from "../queries/category";
-import { Button } from "@material-ui/core";
+import { Button, makeStyles } from "@material-ui/core";
+import clsx from "clsx";
+
+const useStyles = makeStyles((theme) => ({
+  rootContainer: {
+    padding: theme.spacing(2)
+  },
+  homeContainer: {
+    fontSize: 20
+  },
+}))
 
 function Home() {
+  const classes = useStyles();
   const { loading, error, data } = useQuery(CATEGORY_QUERY);
 
   if (loading) return <p>loading...</p>;
   if (error) return <p>error :(</p>;
 
   return (
-    <div className="home">
+    <div className={clsx(classes.rootContainer, classes.homeContainer)}>
       home
       {data.categories.map((category, index) => (
         <div key={index}>
