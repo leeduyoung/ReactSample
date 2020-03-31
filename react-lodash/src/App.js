a77aa8c002616d2b30bd0ea5c98c7d13c59ee2f2
dylee <leeduyoung2002@gmail.com>
2020-03-31 22:59:01 +0900

    bug fix

--- react-lodash/src/App.js
+++ react-lodash/src/App.js
@@ -2,40 +2,44 @@ import React from "react";
 import "./App.css";
 import { debounce } from "lodash";
 
+const somthingFunc = () => {
+  console.log("called somthingFunc");
+};
+
+const debounceSomethingFunc = debounce(() => {
+  console.log("called debounceSomethingFunc");
+}, 200);
+
 function App() {
   const [text, setText] = React.useState("");
   const [text2, setText2] = React.useState("");
 
-  const onChange = value => {
-    console.log("called onChange");
+  const onChange = event => {
+    const value = event.target.value;
+
     setText(value);
+    somthingFunc();
   };
 
-  const onDebounceChange = debounce(value => {
-    console.log("called onDebounceChange");
+  const onDebounceChange = event => {
+    const value = event.target.value;
+
     setText2(value);
-  }, 300);
+    debounceSomethingFunc();
+  };
 
   return (
     <div className="App">
       <div>
         <label>
           <span style={{ marginRight: 16 }}>텍스트1</span>
-          <input
-            type="text"
-            value={text}
-            onChange={event => onChange(event.target.value)}
-          />
+          <input type="text" value={text} onChange={onChange} />
         </label>
       </div>
       <div>
         <label>
           <span style={{ marginRight: 16 }}>텍스트2</span>
-          <input
-            type="text"
-            value={text2}
-            onChange={event => onDebounceChange(event.target.value)}
-          />
+          <input type="text" value={text2} onChange={onDebounceChange} />
         </label>
       </div>
       <div style={{ marginTop: 16 }}>console을 확인해주세요.</div>
