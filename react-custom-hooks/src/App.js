d8d524e885ed64b58a32aef2d0f695ac7a2d1cfc
Jade <leeduyoung2002@gmail.com>
2020-04-09 08:57:20 +0900

    Add input custom hook
    - input custom hook

--- react-custom-hooks/src/App.js
+++ react-custom-hooks/src/App.js
@@ -1,24 +1,21 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { useInput } from './hook/useInput';
 
 function App() {
+  const maxLen = (value) => value.length < 10
+  const name = useInput("", maxLen)
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>
+        Hello
+      </h1>
+
+      <div>
+        <h3>1. useInput</h3>
+        <input placeholder="name" {...name} />
+      </div>
     </div>
   );
 }
---
+++ react-custom-hooks/src/hook/useInput.js
@@ -0,0 +1,24 @@
+import { useState } from "react"
+
+/**
+ * Input Custom Hook
+ * 
+ * @param {*} initialValue 
+ * @param {*} validator 
+ */
+export const useInput = (initialValue, validator) => {
+  const [value, setValue] = useState(initialValue)
+
+  const onChange = event => {
+    const value = event.target.value
+
+    let willUpdate = true
+    if (typeof validator === "function") 
+      willUpdate = validator(value)
+
+    if (willUpdate) 
+      setValue(value)
+  }
+
+  return { value, onChange }
+}
